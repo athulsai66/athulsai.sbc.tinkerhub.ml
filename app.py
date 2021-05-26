dd3a70cd50b767bcf4df2416b9bc9c315b460001
athulsai66 <athulsaii66@gmail.com>
2021-05-26 11:13:19 +0530

    commit 5

--- app.py
+++ app.py
@@ -1,50 +1,51 @@
 from flask import Flask, render_template, request
-from keras.models import load_model
-import os,shutil
-from keras.preprocessing import image
+import sys
+import os
+from numpy.core.records import array
+import tensorflow as tf
 import numpy as np
-from werkzeug.utils import secure_filename
+import matplotlib.pyplot as plt
+from tensorflow.keras.applications.imagenet_utils import decode_predictions, preprocess_input
+from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array, load_img
+sys.path.append(os.path.abspath('./model'))
+from keras.models import load_model
 
+
+app = Flask(__name__)
 model = load_model('model.h5')
 
-app=Flask(__name__)
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-      return render_template("index.html")
+    return render_template('index.html')
 
-
-@app.route("/", methods=["POST"])
+@app.route('/', methods=['POST'])
 def prediction():
+    imagefile= request.files['imagefile']
+    classification = ['','']
+    classification[0]= "images/"+ imagefile.filename
+    image_path = "./static/images/" + imagefile.filename
+    imagefile.save(image_path)
 
-  f= request.files['img']
+    image1 = load_img(image_path, target_size=(224, 224))##loading the image
+    
+    
+    image1 = np.asarray(image1) ##converting to an array
+    image1 = image1 / 255 ##scaling by doing a division of 255
+    plt.imshow(image1)
+    image1 = np.expand_dims(image1, axis=0) ##expanding the dimensions
+    output = model.predict(image1)
+    if ((output[0][0]>0.8000) or (output[0][1]>0.8000)):
+        if output[0][0] > output[0][1]:
+            classification[1] = "Image is of a chakka"##chakka
+        else:
+            classification[1] = "Image is of a manga"##manga
+    else:
+        if output[0][0] > output[0][1]:
+            classification[1] = "Sorry I can't recognise this image but it can be " + str(round((output[0][0]/1)*100)) + "% chakka"##chakka
+        else:
+            classification[1] = "Sorry I can't recognise this image but it can be " + str(round((output[0][1]/1)*100)) + "% manga"##manga
+    return render_template('index.html', prediction=classification)
 
-  folder = r'uploads'
-  for filename in os.listdir(folder):
-    file_path = os.path.join(folder, filename)
-    if os.path.isfile(file_path) or os.path.islink(file_path):
-        os.unlink(file_path)
-    elif os.path.isdir(file_path):
-       shutil.rmtree(file_path)
-
-  
-  basepath = os.path.dirname(__file__)
-  file_path = os.path.join(
-      basepath, 'uploads', secure_filename(f.filename))
-  f.save(file_path)
-  img = image.load_img(file_path,target_size=(224,224)) ##loading the image
-  img = np.asarray(img) ##converting to an array
-  img = img / 255 ##scaling by doing a division of 255
-  img = np.expand_dims(img, axis=0) ##expanding the dimensions
-  output = model.predict(img)
-  if output[0][0] > output[0][1]:
-          result = "chakka"
-  else:
-           result = "manga"
-
-  return render_template("index.html",prediction=result)
-
-
-
-if __name__=="__main__":
-      app.run(debug=True)
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
